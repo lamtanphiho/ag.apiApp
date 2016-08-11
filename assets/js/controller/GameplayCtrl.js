443657bd0ddb2e49c00a3af51b0229dff6d65cd9
tai <lamtanphiho1@gmail.com>
2016-08-12 00:27:23 +0700

    a

--- assets/js/controller/GameplayCtrl.js
+++ assets/js/controller/GameplayCtrl.js
@@ -2,6 +2,6 @@ ag_apiApp.controller('GameplayCtrl', ['$scope', '$sce', function($scope, $sce) {
   $scope.trustSrc = function(src) {
     return $sce.trustAsResourceUrl(src);
   }
-  $scope.api_game = {src:"https://docs.angularjs.org", title:"test aframe"};
+  $scope.api_game = {src:"https://csnbo.w88.com/cas/?op=HOKIBET188", title:"test aframe"};
     
 }]);
